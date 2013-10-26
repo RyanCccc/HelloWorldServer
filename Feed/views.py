04cb66606bef5b078281a43005a80210f81977e7
RyanCccc <chenrd769@gmail.com>
2013-10-26 16:00:09 -0400

    Added post feed

--- Feed/views.py
+++ Feed/views.py
@@ -1,10 +1,32 @@
+import json
+
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
+from Feed.models import Feed, Reply
 
 # Create your views here.
 def create(request):
-    pass
+    post_json = json.loads(request.body)
+    try:
+        user = User.objects.get(username=post_json.get('username'))
+        content = post_json.get('content')
+        latitude = post_json.get('latitude')
+        longitude = post_json.get('longitude')
+        likes = post_json.get('likes')
+        dislikes = post_json.get('dislikes')
+        feed = Feed.objects.create(
+            user = user,
+            content = content,
+            latitude = latitude,
+            longitude = longitude,
+            likes = likes,
+            dislikes = dislikes,
+        )
+        return HttpResponse(feed.pk)
+    except Exception as e:
+        print e
+        return HttpResponse(-1)
 
 
 def get(request):
